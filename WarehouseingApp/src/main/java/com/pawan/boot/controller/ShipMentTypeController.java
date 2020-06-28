b2f60be8faf84c3c374912c18bfb52fa2490baf1
pawan-pc <pawan@gmail>
2020-06-28 21:45:39 +0530

    28/6/20

--- WarehouseingApp/src/main/java/com/pawan/boot/controller/ShipMentTypeController.java
+++ WarehouseingApp/src/main/java/com/pawan/boot/controller/ShipMentTypeController.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +26,7 @@ import com.pawan.boot.view.ShipmentTypeExcelView;
 @RequestMapping("/shipMenttype")
 public class ShipMentTypeController {
 
+	private static final Logger log=LoggerFactory.getLogger(ShipMentTypeController.class);
 	@Autowired
 	private IShipmentTypeService service;
 
